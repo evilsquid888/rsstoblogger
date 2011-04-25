a822f299e75a3a472028c05b08d79e7249779a2a
spicysquid <tom@gogii.net>
2011-04-24 21:47:49 -0700

    get rid of a bat utf 8 character

--- src/main/java/com/blogspot/huegelmeyer/rss2blogger/utils/htmlencode.java
+++ src/main/java/com/blogspot/huegelmeyer/rss2blogger/utils/htmlencode.java
@@ -1,6 +1,6 @@
 /*
  * rss2blogger
- * Copyright (C) 2008 Philipp HŸgelmeyer
+ * Copyright (C) 2008 Philipp Hgelmeyer
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
